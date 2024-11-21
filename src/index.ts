ca8ba7cb6cba104b9c6745ea1455a43e0d1dc859
Kunaldevxxx <khareji594@gmail.com>
2024-11-21 22:59:58 +0530

    bugs solved 1.0.2 deployed ..

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { default as Field } from './components/Field';
+export { default as FormFieldWrapper } from './components/FormFieldWrapper';
+export { default as FormGenerator } from './components/FormGenerator';
+export { default as SelectField } from './components/SelectField';
